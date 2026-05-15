d60b07c78022a3c1a33f444c2f3a0f18ed31341b
luu anh tai <anhtai.luu2703@gmail.com>
2026-05-15 11:22:30 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import React from 'react'
 import { HashRouter } from 'react-router-dom'
 import { MotionConfig } from 'framer-motion'
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <HashRouter basename="/vnits-app">
-      <MotionConfig transition={{ duration: 0.5, ease: "easeInOut" }}>
-        <App />
-      </MotionConfig>
-    </HashRouter>
-  </React.StrictMode>,
+  <HashRouter basename="/vnits-app">
+    <MotionConfig transition={{ duration: 0.5, ease: "easeInOut" }}>
+      <App />
+    </MotionConfig>
+  </HashRouter>
 )
