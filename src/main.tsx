12f09d7446ff7c38127d5480de365f138690c6fa
luu anh tai <anhtai.luu2703@gmail.com>
2026-05-15 11:00:00 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { MotionConfig } from 'framer-motion'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <HashRouter >
       <MotionConfig transition={{ duration: 0.5, ease: "easeInOut" }}>
         <App />
       </MotionConfig>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
