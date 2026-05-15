75001dad581e6129c213ea9b4a64ef2c6571d037
luu anh tai <anhtai.luu2703@gmail.com>
2026-05-15 11:41:42 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { MotionConfig } from 'framer-motion'
 
 createRoot(document.getElementById('root')!).render(
-  <HashRouter>
+  <BrowserRouter>
     <MotionConfig transition={{ duration: 0.5, ease: "easeInOut" }}>
       <App />
     </MotionConfig>
-  </HashRouter>
+  </BrowserRouter>
 )
