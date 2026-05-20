dee77626c8673de1cec5050210b41979eefb9bf5
luu anh tai <anhtai.luu2703@gmail.com>
2026-05-20 10:49:44 +0700

    update

--- src/Components/SolutionHeader.tsx
+++ src/Components/SolutionHeader.tsx
@@ -1,4 +1,5 @@
 // components/SolutionHeader.tsx
+import { Link } from "react-router";
 import { iconMap } from "./icons/icons";
 import "./Styles/SolutionHeader.css";
 
@@ -21,13 +22,13 @@ export default function SolutionHeader({ title, desc, bg, icon }: Props) {
             <div className="solution-header-container">
                 <div className="solution-header-content">
                     
-                    <a className="inline-flex items-center gap-2 text-white/50 hover:text-white transition-colors text-sm font-medium mb-8" href="/#systems" data-discover="true" style={{ textDecoration: "none", paddingBottom: "30px" }}>
+                    <Link className="inline-flex items-center gap-2 text-white/50 hover:text-white transition-colors text-sm font-medium mb-8" to="/" data-discover="true" style={{ textDecoration: "none", paddingBottom: "30px" }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-arrow-left w-4 h-4" aria-hidden="true">
                             <path d="m12 19-7-7 7-7"></path>
                             <path d="M19 12H5"></path>
                         </svg>
                         TRỞ VỀ DANH SÁCH
-                    </a>
+                    </Link>
                     {Icon && (
                         <div className="solution-header-icon">
                             <Icon size={22}/>
